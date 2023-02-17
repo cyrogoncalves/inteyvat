49e0b08746bb45981f689bda994fe6761a12e7be
cyro <cyro.goncalves@globalhitss.com.br>
2023-02-17 18:48:00 -0300

    organize hex utility

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,18 @@
 import * as PIXI from "pixi.js";
-import {Hex, omastar} from "./omastar";
+import * as hex from "./omastar";
 
-type AvatarEntity = { hex: {q:number, r:number}, sprite: PIXI.Sprite, x:number, y:number }
+export type HexGridEntity = { hex: {q:number, r:number}, sprite: PIXI.Sprite, x:number, y:number }
 
-const SIZE = 28;
-
-const axialRound = (x, y) => {
-  const xGrid = Math.round(x), yGrid = Math.round(y);
-  x -= xGrid; y -= yGrid; // remainder
-  const dx = Math.round(x + 0.5*y) * (x*x >= y*y ? 1 : 0);
-  const dy = Math.round(y + 0.5*x) * (x*x < y*y ? 1 : 0);
-  return {q:xGrid + dx, r:yGrid + dy};
-}
-
-const pixelToPointyHex = (x, y) => axialRound((Math.sqrt(3)/3*x - 1./3*y)/SIZE, 2./3*y/SIZE);
-
-const pointyHexToPixel = ({q,r}) => ({x: SIZE*(Math.sqrt(3)*q + Math.sqrt(3)/2*r), y: SIZE*1.5*r});
-
-const sameHex = (h1, h2) => h1?.q === h2.q && h1?.r === h2.r;
-
-const drawHex = (path, { x, y }: {x:number, y:number}, color: number = 0xFFFFFF, size: number = SIZE) => {
-  const points = [0,1,2,3,4,5].map(i => ({
-    x: x + size * Math.cos(Math.PI / 180 * (60 * i - 30)),
-    y: y + size * Math.sin(Math.PI / 180 * (60 * i - 30))
-  }));
+const drawHex = (path, { x, y }: {x:number, y:number}, color: number = 0xFFFFFF) => {
+  const points = hex.vertexesFor(x, y)
   path.clear().lineStyle(3, color, .2).moveTo(points[5].x, points[5].y);
   points.forEach(p => path.lineTo(p.x, p.y));
 }
 
+const drawPath = (path, realPath, {x,y}=team[cur]) => {
+  realPath.clear().lineStyle(2, 0xFFFFFF, 1).moveTo(x, y);
+  path.map(p => hex.toCenterPixel(p)).forEach(p => realPath.lineTo(p.x, p.y));
+}
 
 const app = new PIXI.Application({ width: 1000, height: 600,
   backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1 });
@@ -48,28 +33,31 @@ const selectedHexPath = new PIXI.Graphics();
 app.stage.addChild(selectedHexPath);
 
 const realPath = new PIXI.Graphics();
-container.addChild(realPath)
+container.addChild(realPath);
+
+const curHexPath = new PIXI.Graphics();
+app.stage.addChild(curHexPath);
+
+let cur = 0;
 let path = null;
 let goal = null;
 let follow = false;
 let goalEntity = null;
 let moveCount = 0;
-container.on('pointerdown', ev => {
-  const newGoal = pixelToPointyHex(ev.data.global.x, ev.data.global.y);
-  if (sameHex(goal, newGoal)) return (follow = true); // second click on goal
-  goal = newGoal;
-  if (team.some(t => sameHex(goal, t.hex))) return; // clicked on a char
 
-  goalEntity = entities.find(it => sameHex(goal, it.hex));
+container.on('pointerdown', ev => {
+  const newGoal = hex.from(ev.data.global.x, ev.data.global.y);
+  if (hex.sameCell(goal, newGoal)) return (follow = true); // second click on goal
+  goal = newGoal;
+  if (team.some(t => hex.sameCell(goal, t.hex))) return; // clicked on a char
+  goalEntity = entities.find(it => hex.sameCell(goal, it.hex));
+  const obstacles = entities.map(t=>t.hex).filter(it=>!hex.sameCell(it, goal));
+  path = hex.omastar(team[cur].hex, goal, obstacles);
+
   const color = goalEntity ? 0xFF6666 : 0xFFFF66;
-  drawHex(selectedHexPath, pointyHexToPixel(goal), color);
-  path = omastar(team[cur].hex, goal, entities.map(t=>t.hex).filter(it=>!sameHex(it, goal)));
+  drawHex(selectedHexPath, hex.toCenterPixel(goal), color);
   drawPath(path, realPath)
 });
-const drawPath = (path, realPath, {x,y}=team[cur]) => {
-  realPath.clear().lineStyle(2, 0xFFFFFF, 1).moveTo(x, y);
-  path.map(p => pointyHexToPixel(p)).forEach(p => realPath.lineTo(p.x, p.y));
-}
 
 const entities = [];
 const addToContainer = (it, scale = 0.5) => {
@@ -79,19 +67,19 @@ const addToContainer = (it, scale = 0.5) => {
   container.addChild(it.sprite);
   entities.push(it);
 }
-const newEntity = (hex: Hex, sourceImg: string): AvatarEntity => ({
+
+const newEntity = (hex: hex.Hex, sourceImg: string): HexGridEntity => ({
   hex,
   sprite: new PIXI.Sprite(PIXI.Texture.from(sourceImg)),
   get x() { return this.sprite.x },
   get y() { return this.sprite.y }
 })
 
-let cur = 0;
-const team: AvatarEntity[] = ["lanka.png", "tartartaglia.png", "morax.png", "walnut.png"]
+const team: HexGridEntity[] = ["lanka.png", "tartartaglia.png", "morax.png", "walnut.png"]
   .map((n, i) => newEntity({ q:i+2, r:3 }, `assets/${n}`));
 team.forEach(t => {
   t.sprite.rotation = 0.06;
-  t.sprite.on('pointerdown', () => {
+  t.sprite.on('pointerdown', () => { // TODO move this to generic onPointerDown
     cur = team.findIndex(c => c === t);
     goal = null;
     selectedHexPath.clear();
@@ -112,22 +100,21 @@ team.forEach(t => {
   .forEach(it => addToContainer(it, 0.25));
 
 // enemies
-const enemies: AvatarEntity[] = [];
+const enemies: HexGridEntity[] = [];
 const hilixu = newEntity({q:8,r:8}, `./assets/hilixu.png`);
 hilixu.sprite.rotation = 0.06
 enemies.push(hilixu);
 addToContainer(hilixu, 0.15)
 
+const loot = [];
+
 const updatePos = (it) => it.forEach(t => {
-  const point = pointyHexToPixel(t.hex);
+  const point = hex.toCenterPixel(t.hex);
   [t.sprite.x, t.sprite.y] = [point.x, point.y];
 })
 updatePos(entities);
-const curHexPath = new PIXI.Graphics();
-app.stage.addChild(curHexPath);
 drawHex(curHexPath, team[cur]);
 
-const loot = [];
 
 const tickers = [
   {elapsed:0.0, speed:60, fn:()=>team.forEach(t => t.sprite.rotation = -t.sprite.rotation)},
@@ -135,7 +122,7 @@ const tickers = [
   {elapsed:0.0, speed:20, fn:()=>{
       if (!follow) return;
       const step = path.shift();
-      if (!sameHex(goalEntity?.hex, step)) {
+      if (!hex.sameCell(goalEntity?.hex, step)) {
         move(step, team);
         drawPath(path, realPath);
         if (path.length !== 0) return;
@@ -145,7 +132,7 @@ const tickers = [
         realPath.clear();
       } else {
         loot.push(goalEntity);
-        entities.splice(entities.findIndex(it=>sameHex(it.hex, goalEntity.hex)), 1);
+        entities.splice(entities.findIndex(it=>hex.sameCell(it.hex, goalEntity.hex)), 1);
         console.log("pick!", entities);
         container.removeChild(goalEntity.sprite);
         inventory.addChild(goalEntity.sprite);
@@ -162,17 +149,17 @@ app.ticker.add(delta => tickers.forEach(t=>{
   if (t.elapsed > t.speed) { t.fn(); t.elapsed = 0.0; }
 }));
 
-const exchangePlaces = (team: AvatarEntity[], idx: number, pos0q: number, pos0r: number): void => {
+const exchangePlaces = (team: HexGridEntity[], idx: number, pos0q: number, pos0r: number): void => {
   [team[idx].hex.q, team[idx].hex.r] = [pos0q, pos0r];
   [team[idx], team[cur]] = [team[cur], team[idx]];
   cur = idx;
 }
 
-const move = ({q, r}: Hex, team: AvatarEntity[]): void => {
+const move = ({q, r}: hex.Hex, team: HexGridEntity[]): void => {
   let [pos0q, pos0r] = [team[cur].hex.q, team[cur].hex.r];
   team[cur].hex = {q:q, r:r};
 
-  const idx = team.findIndex(c=>c!==team[cur] && sameHex(c.hex, team[cur].hex));
+  const idx = team.findIndex(c=>c!==team[cur] && hex.sameCell(c.hex, team[cur].hex));
   if (idx > 0) return exchangePlaces(team, idx, pos0q, pos0r) // if the hex already had a unit, just exchange places
 
   const slices = [team.slice(0, cur).reverse(), team.slice(cur+1)]
--- src/omastar.ts
+++ src/omastar.ts
@@ -1,14 +1,38 @@
-// https://www.redblobgames.com/pathfinding/a-star/introduction.html
+const SIZE = 28;
 
 export type Hex = {q:number, r:number}
 
-const axialDistance = (a, b) =>
+const axialDistance = (a:Hex, b:Hex):number =>
     (Math.abs(a.q - b.q)
     + Math.abs(a.q + a.r - b.q - b.r)
     + Math.abs(a.r - b.r)) / 2
 
+const axialRound = (x:number, y:number): Hex => {
+  const xGrid = Math.round(x), yGrid = Math.round(y);
+  x -= xGrid; y -= yGrid; // remainder
+  const dx = Math.round(x + 0.5*y) * (x*x >= y*y ? 1 : 0);
+  const dy = Math.round(y + 0.5*x) * (x*x < y*y ? 1 : 0);
+  return {q:xGrid + dx, r:yGrid + dy};
+}
+
+// pixelToPointyHex
+export const from = (x:number, y:number, size=SIZE): Hex =>
+  axialRound((Math.sqrt(3)/3*x - 1./3*y)/size, 2./3*y/size);
+
+// pointyHexToPixel
+export const toCenterPixel = ({q,r}: Hex, size=SIZE) =>
+  ({x: size*(Math.sqrt(3)*q + Math.sqrt(3)/2*r), y: size*1.5*r});
+
+export const sameCell = (h1, h2) => h1?.q === h2.q && h1?.r === h2.r;
+
+export const vertexesFor = (x:number, y:number, size=SIZE) => [0,1,2,3,4,5].map(i => ({
+  x: x + size * Math.cos(Math.PI / 180 * (60 * i - 30)),
+  y: y + size * Math.sin(Math.PI / 180 * (60 * i - 30))
+}));
+
 const directions = [[-1,1],[-1,0],[0,-1],[1,-1],[1,0],[0,1]];
 
+// https://www.redblobgames.com/pathfinding/a-star/introduction.html
 const exploreFrontier = (start: Hex, goal: Hex, obstacles: Hex[]): {[p: string]: Hex}|null => {
   const frontier = [start];
   const cameFrom = {};
